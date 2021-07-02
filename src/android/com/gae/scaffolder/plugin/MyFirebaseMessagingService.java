0614fba0852298a58c54b0299075b475e8c2bae4
Arihant <arihant.ekism@gmail.com>
2021-07-02 15:05:10 +0530

    Adding messages without notification also into notification tray

--- src/android/com/gae/scaffolder/plugin/MyFirebaseMessagingService.java
+++ src/android/com/gae/scaffolder/plugin/MyFirebaseMessagingService.java
@@ -59,6 +59,58 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         
         Log.d(TAG, "\tNotification Data: " + data.toString());
         FCMPlugin.sendPushPayload(data);
+        if(remoteMessage.getNotification() == null){
+            addNotificationToTray(remoteMessage, data);
+        }
     }
     // [END receive_message]
+    
+    public void addNotificationToTray(RemoteMessage remoteMessage, Map<String, Object> data) {
+        String title = data.get("title") != null ? data.get("title").toString() : null;
+        String body = data.get("body") != null ? data.get("body").toString() : null;;
+
+        if(title == null && data.get("link") !=null){
+            // Trying to extract title from link (in case of tuya)
+            // TODO: Need to extract it from link.
+            // "link":"tuya://action?a=view&ct=You have a visitor&cc=Battery dootrbell ,someone is ringing the bell. &p={\"media\":13}&devId=d7237b993d524bae47liql&type=doorbell&msgId=d126710f1625217589&ts=1625217589000"
+            title = "Doorbell";
+            body = "Someone at doorbell";
+        }
+        Intent intent = new Intent(this, FCMPluginActivity.class);
+        intent.putExtra("title", title);
+        intent.putExtra("body", body);
+        for (String key : data.keySet()) {
+            Object value = data.get(key);
+            intent.putExtra(key, value.toString());
+        }
+
+        int requestCode = 104; // random
+        PendingIntent pendingIntent =PendingIntent.getActivity(this,
+                requestCode,intent,PendingIntent.FLAG_ONE_SHOT);
+        String channelId =  "alerts";
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder;
+        notificationBuilder =
+                new NotificationCompat.Builder(this, channelId)
+                        .setSmallIcon(_getResource("ic_launcher", "mipmap"))
+                        .setContentTitle(title)
+                        .setContentText(body)
+                        .setAutoCancel(true)
+                        // .setSound(defaultSoundUri)
+                        .setChannelId(channelId)
+                        .setPriority(NotificationCompat.PRIORITY_HIGH)
+                        .setContentIntent(pendingIntent);
+                NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+
+        notificationManager.notify(requestCode, notificationBuilder.build());
+        // [END receive_message]
+    }
+
+    private int _getResource(String name, String type) {
+        String package_name = getApplication().getPackageName();
+        Resources resources = getApplication().getResources();
+        return resources.getIdentifier(name, type, package_name);
+    }
 }
