c3afc7d7ba680b2f01dba5ea447b25b52c555fbf
Chaitanya Mankala <chaithanya.mankala@gmail.com>
2021-08-18 17:01:03 +0530

    Update MyFirebaseMessagingService.java

--- src/android/com/gae/scaffolder/plugin/MyFirebaseMessagingService.java
+++ src/android/com/gae/scaffolder/plugin/MyFirebaseMessagingService.java
@@ -10,6 +10,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
+import android.content.pm.PackageManager;
 
 import androidx.core.app.NotificationCompat;
 
